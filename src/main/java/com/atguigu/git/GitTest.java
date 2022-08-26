c902764000c6928469cad41f51944ba51dc93879
xhm <xhm@xidian.com>
2022-08-26 11:51:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xhm
+ * @create 2022-08-26 11:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
